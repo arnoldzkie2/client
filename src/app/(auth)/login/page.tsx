eefc9832e3d690f5f3f8f09a9f973381f2af3899
arnoldzkie2 <arnoldzkie22@gmail.com>
2023-07-18 12:43:00 +0800

    initial

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,14 @@
+import LoginForm from '@/components/auth/LoginForm'
+import LandingHeader from '@/components/landing/LandingHeader'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <LandingHeader />
+        <LoginForm />
+    </>
+    )
+}
+
+export default page
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,14 @@
+import SignupForm from '@/components/auth/SignupForm'
+import LandingHeader from '@/components/landing/LandingHeader'
+import React from 'react'
+
+const page = () => {
+  return (
+        <>
+        <LandingHeader />
+        <SignupForm />
+        </>
+    )
+}
+
+export default page
